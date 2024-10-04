38cc6d25c425e805b6b069e98175d79ed66e3ceb
Raphaelle Goulong <raphaelle.goulong@gmail.com>
2024-10-04 18:43:03 +0200

    feat : console check

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,61 +1,59 @@
-import { useEffect, useState } from "react";
-import { useData } from "../../contexts/DataContext";
-import { getMonth } from "../../helpers/Date";
+import { useEffect, useState } from 'react'
+import { useData } from '../../contexts/DataContext'
+import { getMonth } from '../../helpers/Date'
 
-import "./style.scss";
+import './style.scss'
 
 const Slider = () => {
-  const { data } = useData();
-  const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
-  );
- // Utilisation de useEffect pour gérer la mise à jour automatique de l'index toutes les 5 secondes
- useEffect(() => {
-  const timer = setTimeout(() => {
-    setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0)); // Incrémente l'index ou revient à zéro s'il atteint la fin
-  }, 5000);
+    const { data } = useData()
+    const [index, setIndex] = useState(0)
+    const byDateDesc = data?.focus.sort((evtA, evtB) =>
+        new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+    )
+    // Utilisation de useEffect pour gérer la mise à jour automatique de l'index toutes les 5 secondes
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0)) // Incrémente l'index ou revient à zéro s'il atteint la fin
+        }, 5000)
 
-  // Nettoyage du timer pour éviter les fuites de mémoire
-  return () => clearTimeout(timer);
-}, [index, byDateDesc]); // Dépendances de l'effet : index et la longueur des données
+        // Nettoyage du timer pour éviter les fuites de mémoire
+        return () => clearTimeout(timer)
+    }, [index, byDateDesc]) // Dépendances de l'effet : index et la longueur des données
 
-  return (
-    <div className="SlideCardList">
-      {byDateDesc?.map((event, idx) => (
-        <>
-          <div
-            key={event.title}
-            className={`SlideCard SlideCard--${
-              index === idx ? "display" : "hide"
-            }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
-            </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={event.id}
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx}
-                  // Vérifie si l'index global correspond à l'index de la carte actuellement rendue
-                />
-              ))}
-            </div>
-          </div>
-        </>
-      ))}
-    </div>
-  );
-};
+    return (
+        <div className="SlideCardList">
+            {byDateDesc?.map((event, idx) => (
+                
+                    <div
+                        key={event.title}
+                        className={`SlideCard SlideCard--${index === idx ? 'display' : 'hide'}`}>
+                        <img src={event.cover} alt="forum" />
+                        <div className="SlideCard__descriptionContainer">
+                            <div className="SlideCard__description">
+                                <h3>{event.title}</h3>
+                                <p>{event.description}</p>
+                                <div>{getMonth(new Date(event.date))}</div>
+                            </div>
+                        </div>
+                    </div>
+                   
+                
+            ))}
+            <div className="SlideCard__paginationContainer">
+                        <div className="SlideCard__pagination">
+                            {byDateDesc?.map((event, radioIdx) => (
+                                <input
+                                    key={event.title}
+                                    type="radio"
+                                    name="radio-button"
+                                    defaultChecked={index === radioIdx}
+                                    // Vérifie si l'index global correspond à l'index de la carte actuellement rendue
+                                />
+                            ))}
+                        </div>
+                    </div>
+        </div>
+    )
+}
 
-export default Slider;
+export default Slider
